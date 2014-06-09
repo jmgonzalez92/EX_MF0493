4911989bfb9fc20b95973dd19781bd620e5d8469
jmgonzalez92 <jmgonzalez92@hotmailes>
2014-06-09 14:20:56 +0200

    Cambio

---
+++ Test/EmpresaManagerTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MF0493_3.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class EmpresaManagerTest
+    {
+        [TestMethod]
+        public void getTest()
+        {
+            MF0493Entities db = new MF0493Entities();
+
+            var data = from empresa in db.Empresas
+                       select empresa;
+
+            Empresa emp = data.First();
+
+            Empresa final = EmpresaManager.get(emp.nif);
+
+            bool resultado = false;
+            if (final.nif != null)
+            {
+                resultado = true;
+            }
+            Assert.IsTrue(resultado);
+        }
+        [TestMethod]
+        public void NuevaTest()
+        {
+            Empresa emp = new Empresa();
+            emp.nif = "1111111A";
+            emp.nombre = "Prueba";
+            emp.email = "Prueba@hotmail.com";
+            emp.telefono = "111111111";
+            emp.usr = "admin";
+
+            EmpresaManager.Nueva(emp);
+
+            Empresa resultado = EmpresaManager.get(emp.nif);
+
+            Assert.AreEqual(resultado.nif, emp.nif);
+        }
+        [TestMethod]
+        public void ModificarTest()
+        {
+            Empresa emp = new Empresa();
+            emp.nif = "1111111A";
+            emp.nombre = "Modificado";
+            emp.email = "Prueba@hotmail.com";
+            emp.telefono = "111111111";
+            emp.usr = "admin";
+
+            EmpresaManager.Modificar(emp);
+
+            Empresa resultado = EmpresaManager.get(emp.nif);
+
+            Assert.AreEqual(resultado.nombre, emp.nombre);
+        }
+
+        [TestMethod]
+        public void EliminarTest()
+        { 
+            string nif = "1111111A";
+            bool borrar = EmpresaManager.Eliminar(nif);
+
+
+
+            Assert.AreEqual(borrar,true);
+
+        }
+    }
+}
---
+++ Test/UserManagerTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MF0493_3.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UserManagerTest
+    {
+        [TestMethod]
+        public void CrearAdminTest()
+        {
+            string username = "admin";
+            UserManager.CrearAdmin();
+
+            Usuario  u = UserManager.get(username);
+
+            Assert.AreEqual(u.username, "admin" , "No existe un usuario Admin");
+        }
+
+        [TestMethod]
+        public void getAllTest()
+        {
+            MF0493Entities db = new MF0493Entities();
+            var data = from usuarios in db.Usuarios
+                       select usuarios;
+
+            int totala = data.Count();
+            
+
+            List<Usuario> lst = UserManager.getAll();
+
+            int totalb = lst.Count();
+
+            Assert.AreEqual(totala, totalb);
+        } 
+    }
+}
